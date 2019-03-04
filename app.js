b249168e94033d595d694f5f94570f7c05cf5120
GZZsFather <490906019@qq.com>
2019-03-05 02:01:55 +0800

    version 1 there is still a bug in then()

---
+++ app.js
@@ -0,0 +1,9 @@
+var Promise = require('./core')
+
+
+var promise = new Promise(function(value){
+
+    return resolve(3);
+})
+
+// promise.then(value=>{console.log(value)})
--- core.js
+++ core.js
@@ -1,21 +1,25 @@
 
 // frame of the Promise
 function Promise(fn){
-
-    var self = this;
     
     // 0 -- pending
     // 1 -- resolved
     // 2 -- rejected
-    self._status = 0;
+    this._status = 0;
 
     // value contains the result of the Promise
     // such as value from resolve or reason from reject
-    self._value = null;
+    this._value = null;
 
     // the array for callback functions in then()
-    self._deferreds = [];
+    this._deferreds = [];
 
+    this.resolve = function(value){
+        resolve(this,value);
+    }
+    this.reject = function(reason){
+        reject(this,reason)
+    }
     // The Promise runs like new Promise(function(resolve, reject){...})
     // resolve is like resolve(promise, value), so here use an anoynomous function to perform the function
     // reject is like reject(promise, reason), so here is the reason as the parameter
@@ -52,7 +56,6 @@ Promise.prototype.then = function(onFulfilled, onRejected){
     //if the status is not pending
     // use handleResolved to execute the callback functions
     handleResolved(this, deferred);
-
     // return a Promise to maintain the chain of then()
     return res;
 
@@ -66,3 +69,104 @@ function Handler(onFulfilled, onRejected, res){
     return res; 
 }
 
+
+// function of resolvem to set the statue of a promise into solved
+// but according to the Promise/A+, it the value is a Promise
+// Promise Resolution Procedure must be executed
+function resolve(promise, value){
+
+    // only status pending(0) can be solved
+    if(promise._status !== 0){
+        return;
+    }
+    // Throw error when the value and promise reference to the same Promise
+    if(promise === value){
+        reject(promise, new TypeError("You can not resolve a Promise itself!"))
+    }
+
+    // if value is an Object Promise, then the parameter promise should accept the value
+    if(value && value instanceof Promise && value.then === promise.then){
+        var defereds = promise._deferreds;
+
+        // if the value is still pending, then push all the callbacks into the defereds
+        // to suspend them
+        if(value._status == 0){
+            value._deferreds.push(...defereds);
+        }
+        // if the value is resolved or rejected
+        else if(defereds.length !== 0){
+            for (let i = 0; i < deferreds.length; i++) {
+                handleResolved(value, deferreds[i]);
+              }
+              value._deferreds = [];
+        }
+        return;
+    }
+
+    // if value is Object or Function will be finished sooner or later
+
+    // to get the promise solved
+    promise._status = 1;
+    promise._value = value;
+
+
+    if (promise._deferreds.length !== 0) {
+        for (let i = 0; i < promise._deferreds.length; i++) {
+          handleResolved(promise, promise._deferreds[i]);
+        }
+        // clearing the callbacks of then
+        promise._deferreds = [];
+      }
+
+}
+
+
+// the function of reject a Promise
+function reject(promise, reason){
+    // promise can just be pending
+
+    console.log(promise)
+    if(promise._status !== 0){
+        return;
+    }
+
+    // set the status into resolved
+    promise._status = 2;
+    promise._value =reason;
+
+    if(promise._deferreds.length !== 0){
+        for(let i = 0; i < promise._deferreds.length; i ++ ){
+            handleResolved(promise, promise._deferreds[i]);
+        }
+        promise._deferreds = [];
+    }
+}
+
+
+function handleResolved (promise, deferred) {
+    // 异步执行注册回调
+      var cb = promise._status === 1 ? 
+              deferred.onFulfilled : deferred.onRejected;
+  
+      // 传递注册回调函数为空情况
+      if (cb === null) {
+        if (promise._status === 1) {
+          resolve(deferred.promise, promise._value);
+        } else {
+          reject(deferred.promise, promise._value);
+        }
+        return;
+      }
+  
+      // 执行注册回调操作
+      try {
+        var res = cb(promise._value);
+      } catch (err) {
+        reject(deferred.promise, err);
+      }
+  
+      // 处理链式 then(..) 注册处理函数调用
+      resolve(deferred.promise, res);
+  }
+
+module.exports = Promise
